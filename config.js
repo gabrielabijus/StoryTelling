ef59efe53dda029f5f7d5f1ee613f3b0c0759f08
gabrielabijus <95724163+gabrielabijus@users.noreply.github.com>
2023-06-07 18:53:24 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBpdnMQifh8lmWw1oaeNi8_JjtrWVAd7RU",
+    authDomain: "appnarracaodehistorias-323ce.firebaseapp.com",
+    projectId: "appnarracaodehistorias-323ce",
+    storageBucket: "appnarracaodehistorias-323ce.appspot.com",
+    messagingSenderId: "260495903314",
+    appId: "1:260495903314:web:1690c01a348e23bb03ed8b"
+  };
+
